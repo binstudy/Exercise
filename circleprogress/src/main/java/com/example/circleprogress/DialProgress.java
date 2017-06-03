33065d67dd12a7f2ec3e80feba77713d68ce39d2
liubin-zzh <liubin@zhizihua.com>
2017-06-03 19:02:51 +0800

    自定义DialProgress View 带刻度的进度条

--- circleprogress/src/main/java/com/example/circleprogress/DialProgress.java
+++ circleprogress/src/main/java/com/example/circleprogress/DialProgress.java
@@ -2,10 +2,13 @@ package com.example.circleprogress;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.content.res.Resources;
+import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.RectF;
+import android.graphics.Typeface;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.view.View;
@@ -86,17 +89,111 @@ public class DialProgress extends View {
         mCenterPoint = new Point();
         mDefaultSize = MiscUtil.dipToPx(context, Constant.DEFAULT_SIZE);
         mRectF = new RectF();
-        initConfig();
+        initConfig(context, attrs);
         initPaint();
     }
 
-    private void initConfig() {
+    private void initConfig(Context context, AttributeSet attrs) {
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DialProgress);
+        antiAlias = typedArray.getBoolean(R.styleable.DialProgress_antiAlias, Constant.ANTI_ALIAS);
+        mMaxValue = typedArray.getFloat(R.styleable.DialProgress_maxValue, Constant.DEFAULT_MAX_VALUE);
+        mValue = typedArray.getFloat(R.styleable.DialProgress_value, Constant.DEFAULT_VALUE);
+        mValueSize = typedArray.getFloat(R.styleable.DialProgress_valueSize, Constant.DEFAULT_VALUE_SIZE);
+        mValueColor = typedArray.getColor(R.styleable.DialProgress_valueColor, Color.BLACK);
+        mDialIntervalDegree = typedArray.getInt(R.styleable.DialProgress_dialIntervalDegree, 10);
+        int precision = typedArray.getInt(R.styleable.DialProgress_precision, 0);
+        mPrecisionFormat = MiscUtil.getPrecisionFormat(precision);
+
+        mUnit = typedArray.getString(R.styleable.DialProgress_unit);
+        mUnitColor = typedArray.getColor(R.styleable.DialProgress_unitColor, Color.BLACK);
+        mUnitSize = typedArray.getDimension(R.styleable.DialProgress_unitSize, Constant.DEFAULT_UNIT_SIZE);
+
+        mHint = typedArray.getString(R.styleable.DialProgress_hint);
+        mHintColor = typedArray.getColor(R.styleable.DialProgress_hintColor, Color.BLACK);
+        mHintSize = typedArray.getDimension(R.styleable.DialProgress_hintSize, Constant.DEFAULT_HINT_SIZE);
+
+        mArcWidth = typedArray.getDimension(R.styleable.DialProgress_arcWidth, Constant.DEFAULT_ARC_WIDTH);
+        mStartAngle = typedArray.getFloat(R.styleable.DialProgress_startAngle, Constant.DEFAULT_START_ANGLE);
+        mSweepAngle = typedArray.getFloat(R.styleable.DialProgress_sweepAngle, Constant.DEFAULT_SWEEP_ANGLE);
+
+        mAnimTime = typedArray.getInt(R.styleable.DialProgress_animTime, Constant.DEFAULT_ANIM_TIME);
+
+        mBgArcColor = typedArray.getColor(R.styleable.DialProgress_bgArcColor, Color.GRAY);
+        mDialWidth = typedArray.getDimension(R.styleable.DialProgress_dialWidth, 2);
+        mDialColor = typedArray.getColor(R.styleable.DialProgress_dialColor, Color.WHITE);
+
+        mTextOffsetPercentInRadius = typedArray.getFloat(R.styleable.DialProgress_textOffsetPercentInRadius, 0.33f);
+
+        int gradientArcColors = typedArray.getResourceId(R.styleable.DialProgress_arcColors, 0);
+        if (gradientArcColors != 0) {
+            try {
+                int[] gradientColors = getResources().getIntArray(gradientArcColors);
+                if (gradientColors.length == 0) {
+                    int color = getResources().getColor(gradientArcColors);
+                    mGradientColors = new int[2];
+                    mGradientColors[0] = color;
+                    mGradientColors[1] = color;
+                } else if (gradientColors.length == 1) {
+                    mGradientColors = new int[2];
+                    mGradientColors[0] = gradientColors[0];
+                    mGradientColors[1] = gradientColors[0];
+                } else {
+                    mGradientColors = gradientColors;
+                }
+            } catch (Resources.NotFoundException e) {
+                e.printStackTrace();
+            }
+        }
 
     }
 
     private void initPaint() {
+        mHintPaint = new TextPaint();
+        // 设置抗锯齿,会消耗较大资源，绘制图形速度会变慢。
+        mHintPaint.setAntiAlias(antiAlias);
+        // 设置绘制文字大小
+        mHintPaint.setTextSize(mHintSize);
+        // 设置画笔颜色
+        mHintPaint.setColor(mHintColor);
+        // 从中间向两边绘制，不需要再次计算文字
+        mHintPaint.setTextAlign(Paint.Align.CENTER);
 
+        mValuePaint = new Paint();
+        mValuePaint.setAntiAlias(antiAlias);
+        mValuePaint.setTextSize(mValueSize);
+        mValuePaint.setColor(mValueColor);
+        mValuePaint.setTypeface(Typeface.DEFAULT_BOLD);
+        mValuePaint.setTextAlign(Paint.Align.CENTER);
+
+        mUnitPaint = new Paint();
+        mUnitPaint.setAntiAlias(antiAlias);
+        mUnitPaint.setTextSize(mUnitSize);
+        mUnitPaint.setColor(mUnitColor);
+        mUnitPaint.setTextAlign(Paint.Align.CENTER);
+
+        mArcPaint = new Paint();
+        mArcPaint.setAntiAlias(antiAlias);
+        mArcPaint.setStyle(Paint.Style.STROKE);
+        mArcPaint.setStrokeWidth(mArcWidth);
+        mArcPaint.setStrokeCap(Paint.Cap.BUTT);
+
+        mBgArcPaint = new Paint();
+        mBgArcPaint.setAntiAlias(antiAlias);
+        mBgArcPaint.setStyle(Paint.Style.STROKE);
+        mBgArcPaint.setStrokeWidth(mArcWidth);
+        mBgArcPaint.setStrokeCap(Paint.Cap.BUTT);
+        mBgArcPaint.setColor(mBgArcColor);
+
+        mDialPaint = new Paint();
+        mDialPaint.setAntiAlias(antiAlias);
+        mDialPaint.setColor(mDialColor);
+        mDialPaint.setStrokeWidth(mDialWidth);
     }
 
-
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        setMeasuredDimension(MiscUtil.measure(widthMeasureSpec, mDefaultSize),
+                MiscUtil.measure(heightMeasureSpec, mDefaultSize));
+    }
 }
