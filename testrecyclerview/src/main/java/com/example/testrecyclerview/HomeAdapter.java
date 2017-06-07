9eee58e86619c20599bf569a31fc387c3afe375e
liubin-zzh <liubin@zhizihua.com>
2017-06-07 19:42:05 +0800

    无

--- testrecyclerview/src/main/java/com/example/testrecyclerview/HomeAdapter.java
+++ testrecyclerview/src/main/java/com/example/testrecyclerview/HomeAdapter.java
@@ -12,102 +12,84 @@ import android.widget.TextView;
 
 import java.util.List;
 
-class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder>
-{
+class HomeAdapter extends RecyclerView.Adapter<HomeAdapter.MyViewHolder> {
 
-	private List<String> mDatas;
-	private LayoutInflater mInflater;
+    private List<String> mDatas;
+    private LayoutInflater mInflater;
 
-	public interface OnItemClickLitener
-	{
-		void onItemClick(View view, int position);
-		void onItemLongClick(View view, int position);
-	}
+    public interface OnItemClickLitener {
+        void onItemClick(View view, int position);
 
-	private OnItemClickLitener mOnItemClickLitener;
+        void onItemLongClick(View view, int position);
+    }
 
-	public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener)
-	{
-		this.mOnItemClickLitener = mOnItemClickLitener;
-	}
-	
+    private OnItemClickLitener mOnItemClickLitener;
 
-	public HomeAdapter(Context context, List<String> datas)
-	{
-		mInflater = LayoutInflater.from(context);
-		mDatas = datas;
-	}
+    public void setOnItemClickLitener(OnItemClickLitener mOnItemClickLitener) {
+        this.mOnItemClickLitener = mOnItemClickLitener;
+    }
 
-	@Override
-	public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType)
-	{
-		MyViewHolder holder = new MyViewHolder(mInflater.inflate(
-				R.layout.item_home, parent, false));
-		return holder;
-	}
+    public HomeAdapter(Context context, List<String> datas) {
+        mInflater = LayoutInflater.from(context);
+        mDatas = datas;
+    }
 
-	@Override
-	public void onBindViewHolder(final MyViewHolder holder, final int position)
-	{
-		holder.tv.setText(mDatas.get(position));
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        MyViewHolder holder = new MyViewHolder(mInflater.inflate(
+                R.layout.item_home, parent, false));
+        return holder;
+    }
 
-		// 如果设置了回调，则设置点击事件
-		if (mOnItemClickLitener != null)
-		{
-			holder.itemView.setOnClickListener(new OnClickListener()
-			{
-				@Override
-				public void onClick(View v)
-				{
-					int pos = holder.getLayoutPosition();
-					mOnItemClickLitener.onItemClick(holder.itemView, pos);
-				}
-			});
-			
-			holder.itemView.setOnLongClickListener(new OnLongClickListener()
-			{
-				@Override
-				public boolean onLongClick(View v)
-				{
-					int pos = holder.getLayoutPosition();
-					mOnItemClickLitener.onItemLongClick(holder.itemView, pos);
-					removeData(pos);
-					return false;
-				}
-			});
-		}
-	}
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+        holder.tv.setText(mDatas.get(position));
 
-	@Override
-	public int getItemCount()
-	{
-		return mDatas.size();
-	}
+        // 如果设置了回调，则设置点击事件
+        if (mOnItemClickLitener != null) {
+            holder.itemView.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemClick(holder.itemView, pos);
+                }
+            });
 
-	public void addData(int position)
-	{
-		mDatas.add(position, "Insert One");
-		notifyItemInserted(position);
-	}
+            holder.itemView.setOnLongClickListener(new OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    int pos = holder.getLayoutPosition();
+                    mOnItemClickLitener.onItemLongClick(holder.itemView, pos);
+                    removeData(pos);
+                    return false;
+                }
+            });
+        }
+    }
 
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
 
-	public void removeData(int position)
-	{
-		mDatas.remove(position);
-		notifyItemRemoved(position);
-	}
+    public void addData(int position) {
+        mDatas.add(position, "Insert One");
+        notifyItemInserted(position);
+    }
 
-	class MyViewHolder extends ViewHolder
-	{
+    public void removeData(int position) {
+        mDatas.remove(position);
+        notifyItemRemoved(position);
+    }
 
-		TextView tv;
+    class MyViewHolder extends ViewHolder {
 
-		public MyViewHolder(View view)
-		{
-			super(view);
-			tv = (TextView) view.findViewById(R.id.id_num);
-		
-		
-		}
-	}
+        TextView tv;
+
+        public MyViewHolder(View view) {
+            super(view);
+            tv = (TextView) view.findViewById(R.id.id_num);
+
+        }
+    }
 }
