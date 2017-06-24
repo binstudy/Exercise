cd513d1caf17991c7b79457ffd55479dfb50ffc6
liubin-zzh <liubin@zhizihua.com>
2017-06-24 15:44:51 +0800

    pathtest

--- pathtest/src/main/java/com/studylbn/www/pathtest/PathTest.java
+++ pathtest/src/main/java/com/studylbn/www/pathtest/PathTest.java
@@ -25,7 +25,7 @@ public class PathTest extends View {
     private void initPaint() {
         mPaint = new Paint();
         mPaint.setColor(Color.BLACK);
-        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStyle(Paint.Style.FILL);
         mPaint.setAntiAlias(true);
         mPaint.setStrokeWidth(10);
     }
@@ -40,9 +40,52 @@ public class PathTest extends View {
 //        path.setLastPoint(200,100);
 //        path.lineTo(200, 0);
 //        path.close();
-        path.addRect(-100, -100, 100, 100, Path.Direction.CW);
-        path.setLastPoint(200, -200);
-        canvas.drawPath(path, mPaint);
+
+//        path.addRect(-100, -100, 100, 100, Path.Direction.CW);
+//        path.setLastPoint(200, -200);
+
+//        path.setFillType(Path.FillType.INVERSE_EVEN_ODD); //设置path为奇偶原则
+//        path.addRect(-200, -200, 200, 200, Path.Direction.CW);
+//        path.addRect(-400, -400, 400, 400, Path.Direction.CCW);
+//        canvas.drawPath(path, mPaint);
+
+        canvas.drawColor(Color.BLUE);
+        Path path1 = new Path();
+        Path path2 = new Path();
+        Path path3 = new Path();
+        Path path4 = new Path();
+        Path path11 = new Path();
+        Path path22 = new Path();
+        Path path33 = new Path();
+        Path path44 = new Path();
+        Path path5 = new Path();
+        Path path55 = new Path();
+
+        path1.addCircle(0, 0, 200, Path.Direction.CW);
+        path2.addRect(0, -200, 200, 200, Path.Direction.CW);
+        path3.addCircle(0, -100, 100, Path.Direction.CW);
+        path4.addCircle(0, 100, 100, Path.Direction.CW);
+
+        path11.addCircle(0, 0, 200, Path.Direction.CW);
+        path22.addRect(0, -200, -200, 200, Path.Direction.CW);
+        path33.addCircle(0, -100, 100, Path.Direction.CW);
+        path44.addCircle(0, 100, 100, Path.Direction.CW);
+        path5.addCircle(0, 100, 17, Path.Direction.CW);
+        path55.addCircle(0, -100, 15, Path.Direction.CW);
+
+        path1.op(path2, Path.Op.DIFFERENCE);
+        path1.op(path3, Path.Op.UNION);
+        path1.op(path4, Path.Op.DIFFERENCE);
+        canvas.drawPath(path1, mPaint);
+
+        mPaint.setColor(Color.WHITE);
+        path11.op(path22, Path.Op.DIFFERENCE);
+        path11.op(path33, Path.Op.DIFFERENCE);
+        path11.op(path44, Path.Op.UNION);
+        path11.op(path55, Path.Op.UNION);
+        canvas.drawPath(path11, mPaint);
+        mPaint.setColor(Color.BLACK);
+        canvas.drawPath(path5, mPaint);
     }
 
     @Override
