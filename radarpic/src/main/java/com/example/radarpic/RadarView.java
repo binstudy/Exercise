3b84ccb33115d9bf6c9bd9fd6d167dc7e64ece4c
liubin-zzh <liubin@zhizihua.com>
2017-07-03 18:52:36 +0800

    调试

---
+++ radarpic/src/main/java/com/example/radarpic/RadarView.java
@@ -0,0 +1,149 @@
+package com.example.radarpic;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LiuBin on 2017/7/3 11:42.
+ */
+
+public class RadarView extends View {
+
+    private int count = 6;
+    private float angle = (float) (Math.PI * 2 / count);
+    private float radius;
+    private int centerX;
+    private int centerY;
+    private String[] titles = {"a", "b", "c", "d", "e", "f"};
+    private double[] data = {100, 60, 60, 60, 100, 50, 10, 20}; //各维度分值
+    private float maxValue = 100;             //数据最大值
+    private Paint mainPaint;                //雷达区画笔
+    private Paint valuePaint;               //数据区画笔
+    private TextPaint textPaint;                //文本画笔
+
+    public RadarView(Context context) {
+        super(context);
+        init();
+    }
+
+    public RadarView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public RadarView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        count = Math.min(titles.length, data.length);
+
+        mainPaint = new Paint();
+        mainPaint.setAntiAlias(true);
+        mainPaint.setColor(Color.GRAY);
+        mainPaint.setStyle(Paint.Style.STROKE);
+
+        valuePaint = new Paint();
+        valuePaint.setAntiAlias(true);
+        valuePaint.setColor(Color.BLUE);
+        valuePaint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+        textPaint = new TextPaint();
+        textPaint.setAntiAlias(true);
+        textPaint.setColor(Color.BLACK);
+        textPaint.setStyle(Paint.Style.FILL);
+        textPaint.setTextSize(20);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        radius = Math.min(h, w) / 2 * 0.9f;
+        centerX = w / 2;
+        centerY = h / 2;
+        postInvalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawPolygon(canvas);
+        drawLines(canvas);
+        drawRegion(canvas);
+    }
+
+    /**
+     * 绘制数值区域
+     *
+     * @param canvas
+     */
+    private void drawRegion(Canvas canvas) {
+        Path path = new Path();
+        for (int i = 0; i < count; i++) {
+            double percent = data[i] / maxValue;
+            float x = (float) (centerX + radius * Math.cos(angle * i) * percent);
+            float y = (float) (centerY + radius * Math.sin(angle * i) * percent);
+            if (i == 0) {
+                path.moveTo(x, y);
+            } else {
+                path.lineTo(x, y);
+            }
+            canvas.drawCircle(x, y, 10, valuePaint);
+        }
+        path.close();
+        valuePaint.setAlpha(255);
+        valuePaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(path, valuePaint);
+        valuePaint.setAlpha(127);
+        valuePaint.setStyle(Paint.Style.FILL);
+        canvas.drawPath(path, valuePaint);
+    }
+
+    /**
+     * 绘制多边形
+     *
+     * @param canvas
+     */
+    private void drawPolygon(Canvas canvas) {
+        Path path = new Path();
+        float r = radius / (count - 1);
+        for (int i = 1; i < count; i++) {
+            path.reset();
+            float curR = r * i;
+            for (int j = 0; j < count; j++) {
+                if (j == 0) {
+                    path.moveTo(centerX + curR, centerY);
+                } else {
+                    float x = (float) (centerX + curR * Math.cos(angle * j));
+                    float y = (float) (centerY + curR * Math.sin(angle * j));
+                    path.lineTo(x, y);
+                }
+            }
+            path.close();
+            canvas.drawPath(path, mainPaint);
+        }
+    }
+
+    /**
+     * 绘制线
+     *
+     * @param canvas
+     */
+    private void drawLines(Canvas canvas) {
+        Path path = new Path();
+        for (int i = 0; i < count; i++) {
+            path.moveTo(centerX, centerY);
+            float x = (float) (centerX + radius * Math.cos(angle * i));
+            float y = (float) (centerY + radius * Math.sin(angle * i));
+            path.lineTo(x, y);
+        }
+        canvas.drawPath(path, mainPaint);
+    }
+}
