d38fed5c7721d01448bb81626541b7eafeb9aa04
liubin-zzh <liubin@zhizihua.com>
2017-07-25 00:50:58 +0800

    searchview

---
+++ pathmeasure/src/main/java/com/example/pathmeasure/SearchView2.java
@@ -0,0 +1,28 @@
+package com.example.pathmeasure;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LiuBin on 2017/7/24 23:59.
+ */
+
+public class SearchView2 extends View {
+
+    public SearchView2(Context context) {
+        super(context);
+    }
+
+    public SearchView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public void init() {
+
+    }
+
+
+}
