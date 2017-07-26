fe86fcb9d65c374bc6fa6bf2fe6f0b812f8d9c64
liubin-zzh <liubin@zhizihua.com>
2017-07-26 19:34:19 +0800

    searchView

--- pathmeasure/src/main/java/com/example/pathmeasure/SearchView2.java
+++ pathmeasure/src/main/java/com/example/pathmeasure/SearchView2.java
@@ -1,6 +1,16 @@
 package com.example.pathmeasure;
 
+import android.animation.Animator;
+import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.PathMeasure;
+import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -9,6 +19,24 @@ import android.view.View;
  */
 
 public class SearchView2 extends View {
+
+    enum State {
+        NONE, STARTING, SEARCHING, ENDING
+    }
+
+    private Paint mPaint;
+    private int mWidth, mHight;
+    private State mCurrentState = State.NONE;
+    private Path path_Circle, path_Search;
+    private PathMeasure mPathMeasure;
+    private int defaultDuration = 2000;
+    private ValueAnimator mStartingAnimator, mSearchingAnimator, mEndingAnimator;
+    private float mAnimatorValue = 0;
+    private ValueAnimator.AnimatorUpdateListener mUpdateListener;
+    private Animator.AnimatorListener mAnimatorListener;
+    private Handler mAnimatorHandler;
+    private boolean isOver = false;
+    private int count = 0;
 
     public SearchView2(Context context) {
         this(context, null);
@@ -19,9 +47,164 @@ public class SearchView2 extends View {
         init();
     }
 
-    public void init() {
-
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mWidth = w;
+        mHight = h;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        drawSearch(canvas);
+    }
+
+    public void init() {
+        initPaint();
+        initPath();
+        initListener();
+        initHandler();
+        initAnimator();
+        mCurrentState = State.STARTING;
+        mStartingAnimator.start();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(15);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.WHITE);
+    }
+
+    private void initPath() {
+        mPathMeasure = new PathMeasure();
+        float[] pos = new float[2];
+        RectF r1 = new RectF(-100, -100, 100, 100);
+        RectF r2 = new RectF(-50, -50, 50, 50);
+
+        path_Circle = new Path();
+        path_Circle.addArc(r1, 45, -359.9f);
+
+        path_Search = new Path();
+        path_Search.addArc(r2, 45, 359.9f);
+        mPathMeasure.setPath(path_Search, false);
+        mPathMeasure.getPosTan(0, pos, null);
+        path_Search.lineTo(pos[0], pos[1]);
+    }
+
+    private void initListener() {
+        mUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                mAnimatorValue = (float) animation.getAnimatedValue();
+                invalidate();
+            }
+        };
+
+        mAnimatorListener = new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mAnimatorHandler.sendEmptyMessage(0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animation) {
+
+            }
+        };
+    }
+
+    private void initHandler() {
+        mAnimatorHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (mCurrentState) {
+                    case STARTING:
+                        isOver = false;
+                        mCurrentState = State.SEARCHING;
+                        mStartingAnimator.removeAllListeners();
+                        mSearchingAnimator.start();
+                        break;
+                    case SEARCHING:
+                        if (!isOver) {
+                            mSearchingAnimator.start();
+                            count++;
+                            if (count > 2) {
+                                count = 0;
+                                isOver = true;
+                            }
+                        } else {
+                            mCurrentState = State.ENDING;
+                            mSearchingAnimator.removeAllListeners();
+                            mEndingAnimator.start();
+                        }
+                        break;
+                    case ENDING:
+                        mCurrentState = State.NONE;
+                        break;
+                }
+            }
+        };
+    }
+
+    private void initAnimator() {
+        mStartingAnimator = ValueAnimator.ofFloat(0, 1).setDuration(defaultDuration);
+        mSearchingAnimator = ValueAnimator.ofFloat(0, 1).setDuration(defaultDuration);
+        mEndingAnimator = ValueAnimator.ofFloat(1, 0).setDuration(defaultDuration);
+
+        mStartingAnimator.addUpdateListener(mUpdateListener);
+        mSearchingAnimator.addUpdateListener(mUpdateListener);
+        mEndingAnimator.addUpdateListener(mUpdateListener);
+
+        mStartingAnimator.addListener(mAnimatorListener);
+        mSearchingAnimator.addListener(mAnimatorListener);
+        mEndingAnimator.addListener(mAnimatorListener);
+    }
+
+    private void drawSearch(Canvas canvas) {
+        canvas.translate(mWidth / 2, mHight / 2);
+        canvas.drawColor(Color.parseColor("#0082D7"));
+        switch (mCurrentState) {
+            case NONE:
+                canvas.drawPath(path_Search, mPaint);
+                break;
+            case STARTING:
+                Path dst1 = new Path();
+                mPathMeasure.setPath(path_Search, false);
+                mPathMeasure.getSegment(mPathMeasure.getLength() * mAnimatorValue, mPathMeasure.getLength(), dst1, true);
+                canvas.drawPath(dst1, mPaint);
+                break;
+            case SEARCHING:
+                Path dst2 = new Path();
+                mPathMeasure.setPath(path_Circle, false);
+                float stop = mPathMeasure.getLength() * mAnimatorValue;
+                //下面这句没算明白QQQ
+                float start = (float) (stop - ((0.5 - Math.abs(mAnimatorValue - 0.5)) * mPathMeasure.getLength()));
+                mPathMeasure.getSegment(start, stop, dst2, true);
+                canvas.drawPath(dst2, mPaint);
+                break;
+            case ENDING:
+                Path dst3 = new Path();
+                mPathMeasure.setPath(path_Search, false);
+                //startD、stopD指的是某个距离上的点
+                mPathMeasure.getSegment(mPathMeasure.getLength() * mAnimatorValue, mPathMeasure.getLength(), dst3, true);
+                canvas.drawPath(dst3, mPaint);
+                break;
+        }
+    }
 
 }
