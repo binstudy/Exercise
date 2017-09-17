16ae5d72376b34aaca59ba3624d49416038ccb52
liubin-zzh <liubin@zhizihua.com>
2017-09-18 01:02:35 +0800

    SearchView test

--- pathmeasure/src/main/java/com/example/pathmeasure/SearchView3.java
+++ pathmeasure/src/main/java/com/example/pathmeasure/SearchView3.java
@@ -4,10 +4,13 @@ import android.animation.Animator;
 import android.animation.ValueAnimator;
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathMeasure;
+import android.graphics.RectF;
 import android.os.Handler;
+import android.os.Message;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -59,13 +62,102 @@ public class SearchView3 extends View {
     }
 
     private void init() {
-//        initPaint();
-//        initPath();
-//        initListener();
-//        initHandler();
+        initPaint();
+        initPath();
+        initListener();
+        initHandler();
 //        initAnimator();
         mCurrentState = State.STARTING;
         mStartingAnimator.start();
+    }
+
+    private void initPaint() {
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setStrokeWidth(15);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setColor(Color.WHITE);
+    }
+
+    private void initPath() {
+        mPathMeasure = new PathMeasure();
+        float[] pos = new float[2];
+        RectF r1 = new RectF(-100, -100, 100, 100);
+        RectF r2 = new RectF(-50, -50, 50, 50);
+        path_Circle = new Path();
+        path_Circle.addArc(r1, 45, -359.9f);
+        path_Search = new Path();
+        path_Search.addArc(r2, 45, 359.9f);
+        mPathMeasure.setPath(path_Circle, false);
+        mPathMeasure.getPosTan(0, pos, null);
+        path_Search.lineTo(pos[0], pos[1]);
+    }
+
+    private void initListener() {
+        mUpdateListener = new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator valueAnimator) {
+                mAnimatorValue = (float) valueAnimator.getAnimatedValue();
+                invalidate();
+            }
+        };
+
+        mAnimatorListener = new Animator.AnimatorListener() {
+            @Override
+            public void onAnimationStart(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animator animator) {
+                mAnimatorHandler.sendEmptyMessage(0);
+            }
+
+            @Override
+            public void onAnimationCancel(Animator animator) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animator animator) {
+
+            }
+        };
+    }
+
+    private void initHandler() {
+        mAnimatorHandler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+                switch (mCurrentState) {
+                    case STARTING:
+                        isOver = false;
+                        mCurrentState = State.SEARCHING;
+                        mStartingAnimator.removeAllListeners();
+                        mSearchingAnimator.start();
+                        break;
+                    case SEARCHING:
+                        if (!isOver) {
+                            mSearchingAnimator.start();
+                            count++;
+                            if (count > 2) {
+                                count = 0;
+                                isOver = true;
+                            }
+                        } else {
+                            mCurrentState = State.ENDING;
+                            mSearchingAnimator.removeAllListeners();
+                            mEndingAnimator.start();
+                        }
+                        break;
+                    case ENDING:
+                        mCurrentState = State.NONE;
+                        break;
+                }
+            }
+        };
     }
 
     private void drawSearch(Canvas canvas) {
