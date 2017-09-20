a8a57ee187b0e3ccc1cb48af5ad35f8a29c1c866
liubin-zzh <liubin@zhizihua.com>
2017-09-21 00:32:27 +0800

    GestureDetector

--- pathmeasure/src/main/java/com/example/pathmeasure/MainActivity.java
+++ pathmeasure/src/main/java/com/example/pathmeasure/MainActivity.java
@@ -2,8 +2,14 @@ package com.example.pathmeasure;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    GestureDetector gestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +22,44 @@ public class MainActivity extends AppCompatActivity {
 //                sv.init();
 //            }
 //        });
+        gestureDetector = new GestureDetector(this, new MyOnGestureListener());
+        findViewById(R.id.tv_tv).setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                gestureDetector.onTouchEvent(motionEvent);
+                return true;
+            }
+        });
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        VelocityTracker velocityTracker = VelocityTracker.obtain();
+        velocityTracker.addMovement(event);
+        velocityTracker.computeCurrentVelocity(1000);
+        int xVelocity = (int) velocityTracker.getXVelocity();
+        int yVelocity = (int) velocityTracker.getYVelocity();
+        Log.e("xy", xVelocity + " " + yVelocity);
+        return super.onTouchEvent(event);
+    }
+
+    class MyOnGestureListener extends GestureDetector.SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            Log.e("onFling","onFling");
+            return false;
+        }
+
+        @Override
+        public boolean onDown(MotionEvent e) {
+            Log.e("onDown","onDown");
+            return false;
+        }
+
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            Log.e("onDoubleTap","onDoubleTap");
+            return false;
+        }
     }
 }
